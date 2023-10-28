49edcfbab88ca6f61fbfb665b399b2bfae51e504
Ecaterina Fonariuc <ecaterinafonariuc@Cutarescus-MacBook-Air.local>
2023-10-28 21:28:59 +0300

    arra2

---
+++ app/privacy/page.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center my-10 md:p-6'>
+        <p className='text-xl font-bold'>Politica de confidențialitate</p>
+        <p className='text-sm text-muted-foreground mb-2'>Ultima actualizare: 28.10.2023</p>
+        <p className='text-sm text-muted-foreground'>Această pagină este destinată să informeze utilizatorii despre politica de confidențialitate a aplicației <span className='font-bold'>Erasmus+ Vote</span>.</p>
+        <p className='text-sm leading-6 text-muted-foreground'>
+1. Date Colectate
+
+Pentru a vă înscrie în concurs și a vă oferi oportunitatea de a participa la programul Erasmus, vom colecta următoarele date personale:
+
+Nume și prenume
+Adresă de e-mail
+Număr de telefon
+Țara de origine
+Instituția de învățământ
+Aceste informații sunt necesare pentru a vă putea asigura ca se va desfășura un concurs corect și transparent. 
+
+2. Scopul Colectării Datelor
+
+Datele dumneavoastră personale vor fi utilizate exclusiv în scopul organizării și gestionării concursului "Pașaport Erasmus". Nu vom utiliza informațiile dumneavoastră în alte scopuri fără consimțământul dumneavoastră.
+
+3. Accesul la Date
+
+Doar membrii echipei de organizare a concursului "Pașaport Erasmus" vor avea acces la datele dumneavoastră personale. Aceste date nu vor fi partajate cu terțe părți în afara situațiilor prevăzute de lege.
+
+4. Securitatea Datelor
+
+Punem mare accent pe securitatea datelor dumneavoastră personale. Am implementat măsuri tehnice și organizatorice pentru a proteja datele împotriva accesului neautorizat, pierderii sau modificării.
+
+5. Stocarea Datelor
+
+Datele dumneavoastră personale vor fi păstrate doar pe perioada necesară desfășurării concursului și a perioadei de notificare a câștigătorilor. După acest interval, datele vor fi șterse în mod sigur.
+
+6. Drepturile Dumneavoastră
+
+Conform legilor privind protecția datelor, aveți dreptul de a accesa, corecta sau șterge datele dumneavoastră personale. De asemenea, aveți dreptul de a vă opune prelucrării datelor sau de a solicita restricționarea acestora. Pentru a exercita aceste drepturi sau pentru orice întrebări privind Politica de Confidențialitate, vă rugăm să ne contactați la adresa de e-mail stefan.rotari99i@gmail.com.
+
+7. Actualizări ale Politicii de Confidențialitate
+
+Această Politică de Confidențialitate poate fi actualizată periodic pentru a reflecta modificările în practicile noastre privind protecția datelor. Orice actualizări vor fi postate pe această pagină, astfel încât să fiți întotdeauna la curent cu modul în care gestionăm datele dumneavoastră personale.
+
+Vă mulțumim că ați ales să participați la concursul "Pașaport Erasmus". Ne angajăm să protejăm confidențialitatea datelor dumneavoastră personale și să asigurăm că acestea sunt utilizate numai în scopurile specificate mai sus.
+        </p>
+    </div>
+  )
+}
+
+export default page
